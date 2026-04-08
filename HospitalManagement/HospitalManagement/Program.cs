4db319b049475735ad80d5c0615f61ce60585a20
roman <romandadasov13@gmail.com>
2026-04-08 12:37:15 +0400

    commit 3

--- HospitalManagement/HospitalManagement/Program.cs
+++ HospitalManagement/HospitalManagement/Program.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.DependencyInjection;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.UseUrls("http://0.0.0.0:5171");
 
 builder.Services.AddDatabaseServices(builder.Configuration);
 builder.Services.AddIdentityServices(builder.Configuration);
